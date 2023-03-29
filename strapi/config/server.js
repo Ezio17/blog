3429af41cfe1b32b9f02f9308bb1f06f9478ab3f
Mykola Shcherba <xd_ezio@ukr.net>
2023-03-29 18:38:58 +0300

    changed strapi url

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://blog.snaga-solutions.online/strapi',
+  url: 'http://localhost:1337/strapi',
   app: {
     keys: env.array('APP_KEYS'),
   },
