ea5d1e862280d63ad7bb37ad3cfa6ba5ffadb3b3
Mykola Shcherba <xd_ezio@ukr.net>
2023-04-30 23:12:05 +0300

    google analytics was added

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -3,7 +3,26 @@
 
 import DefaultLayout from '~/layouts/Default.vue';
 
-export default function (Vue) {
-  // Set default layout as a global component
+export default function (Vue, { head, isClient }) {
+  const googleAnalyticsId = process.env.GRIDSOME_GOOGLE_ANALYTIC_ID;
+
+  head.script.push({
+    src: `https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`,
+    async: true,
+  });
+
+  function gtag() {
+    // eslint-disable-next-line prefer-rest-params
+    window.dataLayer.push(arguments);
+  }
+
+  if (isClient) {
+    window.dataLayer = window.dataLayer || [];
+
+    gtag('js', new Date());
+    gtag('config', googleAnalyticsId);
+    window.gtag = gtag; // expose gtag function to global scope
+  }
+
   Vue.component('Layout', DefaultLayout);
 }
